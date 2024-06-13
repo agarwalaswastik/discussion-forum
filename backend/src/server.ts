634ef9f3734f797ab967ebb689deadd9b32409de
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-06-13 17:32:34 +0530

    Set up basic server configuration and dependencies
    + express for api
    + helmet for security headers
    + morgan for logging
    + bodyParser for size control
    + mongoose for mongo connectivity
    + multer for file storage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,26 @@
-console.log("Hello, World");
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+/* Configurations */
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const MONGO_URL = process.env.MONGO_URL || console.log("ERROR: No database found");
+const PORT = process.env.PORT || console.log("ERROR: No port found");
+
+if (PORT && MONGO_URL) {
+  mongoose
+    .connect(MONGO_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server started on port: ${PORT}`)))
+    .catch((err) => console.log(`ERROR: ${err}`));
+}
