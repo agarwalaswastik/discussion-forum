d1f24ee634560e6aa05f346ff8b65e66bbc66f68
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-06-16 15:25:24 +0530

    Add colors, favicon, and default profile svg

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#1f2937",
+        darker: "#111827",
+        light: "#d1d5db",
+        lighter: "#e5e7eb",
+      }
+    },
   },
   plugins: [],
 };
